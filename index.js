4730c4797f96ef014e85c76d67c48152df8e2ceb
NowHits <54032670+NowHits@users.noreply.github.com>
2019-08-12 17:29:49 +0000

    Initial commit -Kai

---
+++ index.js
@@ -0,0 +1,38 @@
+//imports
+const express = require('express');
+const app = express();
+const port = 80;
+const hbs = require('express-handlebars');
+const fs = require('fs');
+const colors = require('colors');
+const mongo = require('mongodb').MongoClient;
+const url = 'mongodb://localhost:27017'
+
+//public folder
+app.use(express.static('public'));
+
+mongo.connect(url, (err, client) => {
+  if (err) {
+    console.error(err)
+    return
+  }
+  const db = client.db('nowHits')
+  const collection = db.collection('djs')
+})
+
+app.set('view engine', 'hbs');
+
+app.engine('hbs', hbs( {
+  extname: "hbs",
+  defaultview: "main",
+  layoutsDir: __dirname + "/views/layouts/",
+  partialsDir: __dirname + "/views/partials/"
+}));
+
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: "Home"
+  });
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}!`.cyan));
