70d22a0bd36e6d0d1402be3463f9fa17b2517024
NowHits <54032670+NowHits@users.noreply.github.com>
2019-08-19 15:12:03 +0000

    a bit of styling ~two.zero

--- public/scripts/playpause.js
+++ public/scripts/playpause.js
@@ -1,6 +1,10 @@
 function play() {
   document.getElementById('player').play()
+  document.getElementById('play').style.display = "none";
+  document.getElementById('pause').style.display = "initial";
 }
 function pause() {
   document.getElementById('player').pause()
-} 
+  document.getElementById('pause').style.display = "none";
+  document.getElementById('play').style.display = "initial";
+}
