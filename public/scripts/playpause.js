98778c9eb079fc20cab789b119c5558f1c9454f9
HB Tech <hazzeryog@gmail.com>
2019-09-06 18:32:28 +0100

    Update playpause.js
    Fixed Audio Mixmatch With Text, Pushed With Atom Not Working RN

--- public/scripts/playpause.js
+++ public/scripts/playpause.js
@@ -1,10 +1,10 @@
 function play() {
-  document.getElementById('player').play()
+  document.getElementById("player").muted = false;
   document.getElementById('play').style.display = "none";
   document.getElementById('pause').style.display = "initial";
 }
 function pause() {
-  document.getElementById('player').pause()
+  document.getElementById("player").muted = true;
   document.getElementById('pause').style.display = "none";
   document.getElementById('play').style.display = "initial";
 }
